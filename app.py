500a32dbf148813a076969a76b1930b958261a92
Conor Twomey <conor.twomey1@mycit.ie>
2018-03-20 17:55:22 +0000

    Add nodes page to server. Returning basic data from node table in JSON format

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
 from flask import Flask
+from flask import jsonify
+from buoy_api.postgres_interaction import PostgresInteraction
+from buoy_api.node import Node
+from login_details import DB_USER, DB_NAME, DB_PASSWORD, HOST
 
 app = Flask(__name__)
+database = PostgresInteraction(DB_NAME, DB_USER, DB_PASSWORD, HOST)
 
 @app.route('/')
 def index_page():
     return "Hello World"
+
+@app.route('/nodes')
+def nodes_page():
+    nodes = database.retrieve_all_nodes()
+    node_objects = []
+    for node in nodes:
+        current_node = Node(node[0], node[1], node[2])
+        node_objects.append(current_node)
+        
+    return jsonify(node_objects)
+
+
+
 
 
 if __name__ == '__main__':
