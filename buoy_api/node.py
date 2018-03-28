e685869287819734fdff5d9f51eaf36e41a30610
Conor Twomey <conor.twomey1@mycit.ie>
2018-03-28 17:01:20 +0100

    Remove redundant lines

--- buoy_api/node.py
+++ buoy_api/node.py
@@ -1,9 +1,6 @@
 class Node(dict):
 
     def __init__(self, id, sigfox_id, active):
-        self.id = id
-        self.sigfox_id = sigfox_id
-        self.active = active
-        dict.__init__(self, id=self.id, sigfox_id=self.sigfox_id, active=self.active)
+        dict.__init__(self, id=id, sigfox_id=sigfox_id, active=active)
 
     
