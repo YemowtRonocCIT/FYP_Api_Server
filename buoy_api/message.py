22bd6428bc60d2385a7718bbccfbcf884978c91a
Conor Twomey <conor.twomey1@mycit.ie>
2018-03-28 17:59:51 +0100

    Add node_id to Message class

--- buoy_api/message.py
+++ buoy_api/message.py
@@ -1,8 +1,9 @@
 class Message(dict):
 
     def __init__(self, button_pressed, temp_sensed, vibration_sensed,
-                                                 temp_value, vibration_value):
+                                        temp_value, vibration_value, node_id):
         
         dict.__init__(self, button_pressed=button_pressed, 
             temperature_sensed=temp_sensed, vibration_sensed=vibration_sensed,
-            temperature_value=temp_value, vibration_value=vibration_value)
+            temperature_value=temp_value, vibration_value=vibration_value, 
+            node_id=node_id)
