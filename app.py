5fc31dbda0e9d85baeab8054c0f0447877d2c471
Conor Twomey <conor.twomey1@mycit.ie>
2018-03-28 18:01:29 +0100

    Update flask entrypoint
    Add function to request messages by sigfox_id
    Add DatabaseParser to flask entrypoint, allowing cleaner code

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from flask import jsonify
 from buoy_api.postgres_interaction import PostgresInteraction
 from buoy_api.node import Node
 from buoy_api.message import Message
+from buoy_api.database_parser import DatabaseParser
 from login_details import DB_USER, DB_NAME, DB_PASSWORD, HOST
 
 app = Flask(__name__)
 database = PostgresInteraction(DB_NAME, DB_USER, DB_PASSWORD, HOST)
+database_parser = DatabaseParser()
 
 @app.route('/')
 def index_page():
@@ -22,6 +24,16 @@ def nodes_page():
 
     return jsonify(node_objects)
 
+@app.route('/message')
+def messages_page():
+    rows = database.retrieve_all_messages()
+    messages = []
+    for row in rows:
+        message = database_parser.convert_to_message(row)
+        messages.append(message)
+    
+    return jsonify(messages)
+
 MESSAGE_ID_INDEX = 0
 NODE_ID_INDEX = 1
 BUTTON_PRESSED_INDEX = 2
@@ -30,15 +42,12 @@ VIBRATION_INDEX = 4
 TEMPERATURE_SENSED_INDEX = 5
 VIBRATION_SENSED_INDEX = 6
 
-@app.route('/message')
-def messages_page():
-    rows = database.retrieve_messages_by_sigfox_id('1D1BF9')
+@app.route('/message/<sigfox_id>')
+def messages_by_sigfox_id_page(sigfox_id):
+    rows = database.retrieve_messages_by_sigfox_id(sigfox_id)
     messages = []
     for row in rows:
-        message = Message(row[BUTTON_PRESSED_INDEX], row[VIBRATION_INDEX],
-            row[TEMPERATURE_INDEX], row[TEMPERATURE_SENSED_INDEX], 
-            row[VIBRATION_SENSED_INDEX])
-
+        message = database_parser.convert_to_message(row)
         messages.append(message)
 
     return jsonify(messages)
