58e80b65d40e45e5d2960a6d1de0c6d228c035ed
Conor Twomey <conor.twomey1@mycit.ie>
2018-03-29 13:40:33 +0100

    Change id to node_id in Node class

--- buoy_api/node.py
+++ buoy_api/node.py
@@ -1,6 +1,6 @@
 class Node(dict):
 
     def __init__(self, id, sigfox_id, active):
-        dict.__init__(self, id=id, sigfox_id=sigfox_id, active=active)
+        dict.__init__(self, node_id=id, sigfox_id=sigfox_id, active=active)
 
     
