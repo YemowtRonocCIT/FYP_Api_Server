198d5df453f425728cddda979a934e0e75501cf4
Conor Twomey <conor.twomey1@mycit.ie>
2018-03-31 16:09:26 +0100

    Update indexes on values for database response

--- buoy_api/database_parser.py
+++ buoy_api/database_parser.py
@@ -13,13 +13,13 @@ class DatabaseParser(object):
 
         row: Single row returned from SELECT
         """
-        NODE_ID_INDEX = 1
-        BUTTON_PRESSED_INDEX = 2
-        TEMPERATURE_INDEX = 3
-        VIBRATION_INDEX = 4
-        TEMPERATURE_SENSED_INDEX = 5
-        VIBRATION_SENSED_INDEX = 6
-        TIME_INDEX = 7
+        NODE_ID_INDEX = 0
+        BUTTON_PRESSED_INDEX = 1
+        TEMPERATURE_INDEX = 2
+        VIBRATION_INDEX = 3
+        TEMPERATURE_SENSED_INDEX = 4
+        VIBRATION_SENSED_INDEX = 5
+        TIME_INDEX = 6
 
         message = Message(row[BUTTON_PRESSED_INDEX], row[TEMPERATURE_SENSED_INDEX],
             row[VIBRATION_SENSED_INDEX], row[TEMPERATURE_INDEX], 
